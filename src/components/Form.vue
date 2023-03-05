441c17645be7a8ac6853f8714478f27aa36ef467
ErickBastoCen <erick.19070005@itsmotul.edu.mx>
2023-03-05 17:25:37 -0600

    Actualizacion de diseno y funcionamiento

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,6 @@
 <script>
   import axios from 'axios'
+
   export default {
     name: 'Form',
     data(){
@@ -16,12 +17,14 @@
         personalidad02: [],
         personalidad03: [],
         personalidad04: [],
-        objetivos: [],
-        frustraciones: [],
-        motivaciones: [],
-        marcas: []
+        marca: []
+        //objetivos: [],
+        //frustraciones: [],
+        //motivaciones: [],
+        //marcas: []
         }
-    },
+        },
+
   methods:{
         submit(){
           axios.post("/api/guardarPersonasUxd.php", {
@@ -38,10 +41,11 @@
             personalidad02: this.personalidad02,
             personalidad03: this.personalidad03,
             personalidad04: this.personalidad04,
-            objetivos:this.objetivos,
-            frustraciones: this.frustraciones,
-            motivaciones: this.motivaciones,
-            marcas: this.marcas,
+            marca: this.marca
+            //objetivos:this.objetivos,
+            //frustraciones: this.frustraciones,
+            //motivaciones: this.motivaciones,
+            //marcas: this.marcas,
 
           }).then((response) => {console.log(response.status)});
         }
@@ -81,9 +85,9 @@
         </label>
         <div class="relative">
           <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="estadoCivil" v-model="estadoCivil">
-            <option>Soltero(a)</option>
-            <option>Casado(a)</option>
-            <option>Viudo(a)</option>
+            <option value="1">H</option>
+            <option value="2">F</option>
+            <option value="2">D</option>
           </select>
         </div>
         <p class="text-gray-600 text-xs italic">Introduzca su estado civil.</p>
@@ -184,9 +188,9 @@
     <div class="flex flex-wrap -mx-3 mb-6">
       <div class="w-full px-3">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
-          Marcas
+          Marca
         </label>
-        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="marcas" type="text" placeholder="Coca Cola" v-model="marcas">
+        <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="marcas" type="text" placeholder="Coca Cola" v-model="marca">
         <p class="text-red-500 text-xs italic">Por favor introduzca el autor de su marca.</p>
       </div>
     </div>
@@ -220,17 +224,11 @@
         <p class="text-red-500 text-xs italic">Por favor introduzca sus motivaciones.</p>
       </div>
     </div>
-
-
-
-
-    
   </form>
-
-
 
   <button @click="submit()" type="button"
       class="inline-block rounded-full bg-neutral-800 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-[0_4px_9px_-4px_#332d2d] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.3),0_4px_18px_0_rgba(51,45,45,0.2)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.3),0_4px_18px_0_rgba(51,45,45,0.2)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.3),0_4px_18px_0_rgba(51,45,45,0.2)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#171717] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(27,27,27,0.3),0_4px_18px_0_rgba(27,27,27,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(27,27,27,0.3),0_4px_18px_0_rgba(27,27,27,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(27,27,27,0.3),0_4px_18px_0_rgba(27,27,27,0.2)]">Aceptar</button>
+
 </center>
 </template>
   
